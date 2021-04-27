6e4f9fb53dfca3a223a66bc7fe3ff17393bf949a
vinicius0145 <ninho5@live.com>
2021-04-27 14:59:48 -0300

    reorganizacao

--- plantmanager/src/pages/Welcome.tsx
+++ plantmanager/src/pages/Welcome.tsx
@@ -1,19 +1,18 @@
-import React, { useState } from 'react'
-import { SafeAreaView, View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native'
+import React from 'react'
+import { 
+  SafeAreaView, 
+  Text, 
+  Image, 
+  TouchableOpacity, 
+  Dimensions,
+  StyleSheet } from 'react-native'
 
-import { Button } from '../components/Button'
 
 import wateringImg from '../assets/watering.png'
 import colors from '../styles/colors'
 
 
 export function Welcome(){
-  const [visible, setVisible] = useState(false)
-
-  function handleVisibility(){
-    setVisible(true)
-  }
-
   return (
       <SafeAreaView style={styles.container}>
         <Text style={styles.title}>
@@ -22,17 +21,29 @@ export function Welcome(){
           de forrma fácil
         </Text>
         
-        {
-          visible &&
-         <Image source={wateringImg} style={styles.image} />
-        }
+        <Image 
+          source={wateringImg} 
+          style={styles.image} 
+          resizeMode="contain"
 
+        />
+                
         <Text style={styles.subtitle}>
           Não esqueça mais de regar suas plantas.
           Nós cuidamos de lembrar você sempre que precisar.
         </Text>
 
-        <Button title=">" onPress={handleVisibility}/>
+          <TouchableOpacity 
+            style={styles.button}
+            activeOpacity={0.7}
+            >
+            
+              <Text 
+                style={styles.buttonText}
+                >
+                  >
+              </Text>
+           </TouchableOpacity>
         
       </SafeAreaView>
   )
@@ -42,7 +53,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
   },
   title: {
     color: colors.heading,
@@ -57,11 +68,20 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     color: colors.heading,
   },
-  
   image: {
-    width: 292,
-    height: 284,
+    height: Dimensions.get('window').width * 0.7,
   },
-  
-
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 24,
+  },
 })
